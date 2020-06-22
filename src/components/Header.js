248fa93c4236cdf8abe3703f14f48f979c2fadac
Mustafa <mustafa_creative@hotmail.com>
2020-06-22 09:34:51 +0500

    Basic App For Demo

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const header = () => {
+    return (
+        <h2>
+            Expense Tracker Application (Using Redux & React)
+        </h2>
+    )
+}
+
+export default header;
